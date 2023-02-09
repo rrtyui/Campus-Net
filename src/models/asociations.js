a35025bb128d68005c50e26bf09d99967a06de7e
Facundo1SanAndrea <sanandrea.f@gmail.com>
2023-02-09 17:20:16 +0000

    ading theacer and asociation

--- src/models/asociations.js
+++ src/models/asociations.js
@@ -1,9 +1,12 @@
-const User = require('./users')
-const Asistance = require('./asistances')
-const Group = require('./group')
-const Note = require('./notes')
-const Resource = require('./resources')
-const Sp_resource = require('./specific_resource')
-const Inscription = require('./inscriptions')
+const Student = require('./students');
+const Teacher = require('./teachers');
+const Asistance = require('./asistances');
+const Group = require('./group');
+const Note = require('./notes');
+const Resource = require('./resources');
+const Sp_resource = require('./specific_resource');
+const Inscription = require('./inscriptions');
 
 // defining one-to-many relationship between group
+
+Student.hasMany()

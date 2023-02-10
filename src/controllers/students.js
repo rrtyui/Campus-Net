9d92b29642ec6ade631690ab24c0508f87c7eeb7
Facundo1SanAndrea <sanandrea.f@gmail.com>
2023-02-10 14:56:45 +0000

    change connection mysql

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -5,12 +5,8 @@ const router = express.Router();
 const mysql = require('mysql');
 const { response } = require('express');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'admin',
-    database: 'mysql-server'
-});
+
+
 
 module.exports = router;
 
--- src/pg-db.js
+++ src/pg-db.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
 const { db } = require('./config');
 const { Sequelize } = require('sequelize');
 

db6d3674c8134a9e86bdaeddabfc3801aa41b9a6
Facundo1SanAndrea <sanandrea.f@gmail.com>
2023-02-11 20:34:03 +0000

    adding handeling errors

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -1,14 +1,28 @@
 const Student = require('../models/students');
 
 async function createStudent(name, lastname, email, password) {
-  const student = await Student.create({ 
+  try {
+    const existingStudent = await Student.findOne({
+      where: {
+        email: email,
+        name: name,
+      }
+    });
+
+  if (existingStudent) {
+    throw new Error('Email or Name already in use');
+  }
+  const student = await Student.create({
     first_name : name,
     last_name: lastname,
     email: email,
-    password: password, 
+    password: password,
   });
   console.log(`Student created:`, student.toJSON());
+  } catch (error) {
+    console.error('An Error has ocurred');
+  }
 }
 
-console.log(process.env); // see env variables
-createStudent("jacinto", "Dome", "campeo@gmail.com", "culo2000");
+ console.log(process.env); // see env variables
+ createStudent("jacinto", "Dome", "campeo@gmail.com", "culo2000")

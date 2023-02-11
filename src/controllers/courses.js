c13411798ffa7e564bea70ac528caec159095f30
Facundo1SanAndrea <sanandrea.f@gmail.com>
2023-02-11 20:43:04 +0000

    adding handel error to see is it already created

--- src/controllers/courses.js
+++ src/controllers/courses.js
@@ -1,6 +1,14 @@
 const Courses = require('../models/courses');
 
 async function CreateCourse(student_id, course ) {
+    const exisitngCourse = await Courses.findOne({
+        where: {
+            course: course,
+        }
+    });
+    if (exisitngCourse) {
+        throw new Error('Course already created')
+    }
     const Course = await Courses.create ({
         course : course,
         student_id: student_id,
--- src/controllers/professors.js
+++ src/controllers/professors.js
@@ -1,14 +1,26 @@
 const Profesor = require('../models/professors');
 
 async function createProfessor(name, lastname, email, password) {
-  const profesor = await Profesor.create({ 
-    first_name : name,
-    last_name: lastname,
-    email: email,
-    password: password, 
-  });
+  try {
+    const existingProfesor = await Profesor.findOne({
+      where: {
+        email: email,
+        name: name,
+      }
+    });
+    if (existingProfesor) {
+      throw new Error('Email or Name already in use.');
+    }
+    const profesor = await Profesor.create({
+      first_name : name,
+      last_name: lastname,
+      email: email,
+      password: password,
+    });
   console.log(`Professor created:`, profesor.toJSON());
+  } catch (error) {
+    console.log('An error has ocurred.')
+  }
 }
-
 // console.log(process.env); // see env variables
 createProfessor("Alastor", "Moody", "buzzyogurtlight@gmail.com", "macklemore");

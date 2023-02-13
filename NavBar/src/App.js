b6e43076744ef19396a6f9e2bf69318756f73d6b
Germán "amorcito" Izquierdo <izquierdoz.g@gmail.com>
2023-02-13 16:41:23 -0300

    CRUD PANEL - other changes

--- NavBar/src/App.js
+++ NavBar/src/App.js
@@ -1,32 +1,14 @@
-import React from 'react';
-import Navbar from "./Components/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/home";
-import Alumnos from "./Pages/alumnos";
-import Cursos from "./Pages/cursos";
-import Recursos from "./Pages/recursos";
-import {useEffect, useState} from 'react';
-import axios from 'axios';
-import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
-import { show_alerta } from './functions';
+import { Routes, Route, BrowserRouter} from 'react-router-dom';
+import ShowProducts from './components/ShowProducts';
 
-function App()
-{
-	return (
-		<BrowserRouter>
-			<React.Fragment>
-				<Navbar />
-			</React.Fragment>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/Alumnos" element={<Alumnos />} />
-					<Route path="/Cursos" element={<Cursos />} />
-					<Route path="/Recursos" element={<Recursos />} />
-				</Routes>
-		</BrowserRouter>
-			);
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ShowProducts></ShowProducts>}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
-			
